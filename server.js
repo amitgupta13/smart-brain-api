98be8c68f1dea034932fd733a96f48007c358699
Amit Gupta <amit.gupta@daffodilsw.com>
2019-03-27 13:47:32 +0530

    nc

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const bcrypt = require('bcrypt-nodejs');
+const postgres = require('knex')({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : 'hrhk',
+      database : 'smartbrain'
+    }
+  });
 const app = express();
 
 app.use(cors());
@@ -34,48 +44,66 @@ app.get('/', (req, res)=>{
 })
 
 app.post('/signin', (req, res)=>{
-    if(req.body.email === database.users[0].email 
-        && req.body.password === database.users[0].password) return res.json(database.users[0]);
-            res.status(400).json('error logging in');
+    postgres.select('email', 'hash').from('login')
+        .where('email', '=', req.body.email)
+        .then(data=>{
+            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+            if(isValid) {
+                postgres.select('*').from('users')
+                    .where('email', '=', req.body.email)
+                    .then(user=>res.json(user[0]))
+                    .catch(err=>res.status(400).json(err))
+            }else{
+                return res.status(400).json('wrong credentials');
+            }
+        })
+        .catch(err=>res.status(400).json(err));
  })
 
 app.post('/register', (req, res)=>{
     const {email, name, password} = req.body;
-     database.users.push({
-        id: '3',
-        name,
-        email,
-        password,
-        entries: 0,
-        joined: new Date()
-     });
 
-     res.json(database.users[database.users.length - 1]);
+    const hash = bcrypt.hashSync(password);
+
+    postgres.transaction(trx => {
+         trx.insert({
+            hash,
+            email
+        })
+        .into('login')
+        .returning('email')
+        .then(loginemail=> {
+            trx('users').returning('*').insert({
+                email:loginemail[0],
+                name,
+                joined: new Date()
+            })
+            .then(user=>res.json(user[0]))
+            .then(trx.commit)
+            .catch(trx.rollback)
+        })
+        .catch(err=>res.status(400).json(err))
+
+    })
 })
 
 app.get('/profile/:id', (req, res)=>{
     const {id} = req.params;
-    let found = false;
-    database.users.forEach(user => {
-        if(user.id === id){
-            found = true;
-            return res.json(user);
-        }
-    })
-    if(!found) return res.status(400).json('not found')
+    postgres.select('*').from('users').where({id})
+        .then(user=>{
+            if(user.length) return res.json(user[0]);
+                return res.status(400).json('not found');
+        })
+        .catch(err=>res.status(400).json(err))
 })
 
 app.put('/image', (req, res)=>{
     const {id} = req.body;
-    let found = false;
-    database.users.forEach(user => {
-        if(user.id === id){
-            found = true;
-            user.entries++
-            return res.json(user.entries);
-        }
-    })
-    if(!found) return res.status(400).json('not found')
+    postgres('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries=>res.json(entries[0]))
+        .catch(err=>res.status(200).json(err))
 })
 
 app.listen(4000, ()=>{
